3ff69eac0e45e6fa53bc94142dde776b578f8b96
Tanush Sethi <tanushsethi@Tanushs-MacBook-Air.local>
2022-08-20 23:34:26 +0530

    javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-̀
+function clicked(){
+    
+}
+
+var display = document.getElementById("display-div");
+var keys = document.getElementsByTagName("td");
+
+for(var i = 0; i< keys.length ; i++){
+    keys[i].addEventListener(onclick, clicked);
+}
+
