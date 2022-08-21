251066e796772102c6dafcaf36b5e0bd6aa695ea
Tanush Sethi <tanushsethi@Tanushs-MacBook-Air.local>
2022-08-21 14:33:11 +0530

    javascript changes

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function clicked(){
-    
+    if(this.id == "key-ac"){
+        alert("hello");
+    }
 }
 
 var display = document.getElementById("display-div");
-var keys = document.getElementsByTagName("td");
+var keys = document.getElementsByTagName("span");
 
-for(var i = 0; i< keys.length ; i++){
-    keys[i].addEventListener(onclick, clicked);
-}
+// for(var i = 0; i< keys.length ; i++){
+    keys[0].addEventListener(onclick, clicked);
+// }
 
