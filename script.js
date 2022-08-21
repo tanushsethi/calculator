694f14be6247ee70e4da43c8f0e74e1a89919e8e
Tanush Sethi <tanushsethi@Tanushs-MacBook-Air.local>
2022-08-21 15:48:05 +0530

    working 2

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
 function clicked(event){
     if(event.srcElement.innerText == "AC"){
         display.innerText = "";
-    }else if(event.srcElement.innerText == "="){
+    }else if(event.srcElement.innerText == "+/-"){
+        if(display.innerText.length!=0){
+            if(display.innerText[0]=="-"){
+                display.innerText = display.innerText.slice(1);
+            }else{
+                display.innerText = "-" + display.innerText;
+            }
+        }
+    }
+    else if(event.srcElement.innerText == "="){
         display.innerText = eval(display.innerText);
     }else{
         display.innerText += event.srcElement.innerText;
